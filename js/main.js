05a5b9873d246b8d648013750d016a2ae76a2581
amir cabili <amircabili@hotmail.com>
2022-01-18 17:50:48 +0200

    first

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 
 
 const allBboxes = document.querySelectorAll("#box1, #box2, #box3, #box4, #box5, #box6, #box7");
-// page1.style.display = 'block'
-// page2.style.display = 'none'
 
 allBboxes.forEach(input => input.addEventListener('click', event => {
     console.log("querySelectorAll");
